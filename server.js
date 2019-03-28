d03d35a178dc5c14b6039c4bfad6b5f9b309d756
NLCaceres <NLCaceres21@Gmail.com>
2019-03-28 14:31:07 -0700

    Added in app.listen in server.js. Briefly removed it with last commit.

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
 // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);
