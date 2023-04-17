8018f2e5ff571f9fce674d62e2d678a40c6ffef4
NLCaceres <NLCaceres21@Gmail.com>
2023-04-17 02:29:10 -0700

    Use fakeAsync, of() + explicit typing for simpler tests

--- src/app/add-profession/add-profession.component.spec.ts
+++ src/app/add-profession/add-profession.component.spec.ts
@@ -1,10 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { AddProfessionComponent } from './add-profession.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ProfessionService } from 'app/profession.service';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { By } from '@angular/platform-browser';
-import { Observable } from 'rxjs';
+import { of } from 'rxjs';
 import { Router } from '@angular/router';
 
 describe('AddProfessionComponent', () => {
@@ -38,28 +38,28 @@ describe('AddProfessionComponent', () => {
     expect(serviceSpy).toBeTruthy();
     expect(routerSpy).toBeTruthy();
   });
-  it('should run router.navigate() after back-button is clicked', () => {
+  it('should run router.navigate() after back button is clicked', () => {
     const backButton = fixture.debugElement.query(By.css('.container > button'));
     backButton.triggerEventHandler('click');
     expect(routerSpy.navigate).toHaveBeenCalledOnceWith(['professions']);
   })
   describe('should run add() after the submit button is clicked', () => {
     it('clears text after submission', () => {
-      serviceSpy.addProfession.and.returnValue(new Observable()); //* Simple stub to prevent an undefined return and subscribe()
+      serviceSpy.addProfession.and.returnValue(of()); //* Simple stub to prevent an undefined return and subscribe()
       const submitButton = fixture.debugElement.query(By.css('.form__submit-button'));
-      const occupationInput = fixture.debugElement.query(By.css('input[placeholder~="Occupation"]')).nativeElement;
-      const disciplineInput = fixture.debugElement.query(By.css('input[placeholder~="Discipline"]')).nativeElement;
-
+      const occupationInput: HTMLInputElement = fixture.debugElement.query(By.css('input[placeholder~="Occupation"]')).nativeElement;
+      const disciplineInput: HTMLInputElement = fixture.debugElement.query(By.css('input[placeholder~="Discipline"]')).nativeElement;
+      
       occupationInput.value = 'Foobar'; //* Insert text into occupation input
       disciplineInput.value = 'Barfoo'; //* Insert text into discipline input
-
+      
       submitButton.triggerEventHandler('click');
-
-      expect(occupationInput.textContent).toBe(''); //* Submission done, both inputs made empty before navigate() is called
-      expect(disciplineInput.textContent).toBe('');
+      
+      expect(occupationInput.value).toBe(''); //* Submission done, both inputs made empty before navigate() is called
+      expect(disciplineInput.value).toBe('');
     })
-    it('must have valid data passed in to call addProfession()', () => {
-      serviceSpy.addProfession.and.returnValue(new Observable());
+    it('must have valid data input to call addProfession()', () => {
+      serviceSpy.addProfession.and.returnValue(of());
       const submitButton = fixture.debugElement.query(By.css('.form__submit-button'));
       const occupationInput = fixture.debugElement.query(By.css('input[placeholder~="Occupation"]')).nativeElement;
       const disciplineInput = fixture.debugElement.query(By.css('input[placeholder~="Discipline"]')).nativeElement;
@@ -73,22 +73,21 @@ describe('AddProfessionComponent', () => {
       submitButton.triggerEventHandler('click');
       expect(serviceSpy.addProfession).toHaveBeenCalledTimes(1);
     })
-    it('will run router.navigate() upon successful submission', () => {
-      serviceSpy.addProfession.and.returnValue(new Observable());
+    it('will run router.navigate() upon successful submission', fakeAsync(() => {
+      tick(); //? Must send some value in of() so addProfession's subscribe() can run routerSpy.navigate() later
+      serviceSpy.addProfession.and.returnValue(of({ observedOccupation: 'Foobar', serviceDiscipline: 'Barfoo' }));
       const submitButton = fixture.debugElement.query(By.css('.form__submit-button'));
       const occupationInput = fixture.debugElement.query(By.css('input[placeholder~="Occupation"]')).nativeElement;
       const disciplineInput = fixture.debugElement.query(By.css('input[placeholder~="Discipline"]')).nativeElement;
 
-      submitButton.triggerEventHandler('click'); //* Submitting without any data is invalid!
-      expect(routerSpy.navigate).toHaveBeenCalledTimes(0); //* So no back() called
+      submitButton.triggerEventHandler('click'); //* Submitting empty data is invalid!
+      expect(routerSpy.navigate).toHaveBeenCalledTimes(0); //* So no navigate() called
 
       occupationInput.value = 'Foobar'; //* Insert text into input
       disciplineInput.value = 'Barfoo';
 
       submitButton.triggerEventHandler('click');
-      setTimeout(() => { //* Briefly wait for subscription to run router.navigate()
-        expect(routerSpy.navigate).toHaveBeenCalledOnceWith(['professions']);
-      }, 100);
-    })
+      expect(routerSpy.navigate).toHaveBeenCalledOnceWith(['professions']);
+    }))
   })
 });
