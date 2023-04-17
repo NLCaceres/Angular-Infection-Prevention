c9c2f3851ce65aecc8e1edc6564824f42f5a825c
NLCaceres <NLCaceres21@Gmail.com>
2023-04-16 21:43:57 -0700

    Clean layout + use single quotes + add ngForm

--- src/app/profession-detail/profession-detail.component.spec.ts
+++ src/app/profession-detail/profession-detail.component.spec.ts
@@ -1,23 +1,103 @@
-import { waitForAsync, ComponentFixture, TestBed } from "@angular/core/testing";
-import { ProfessionDetailComponent } from "./profession-detail.component";
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { ProfessionDetailComponent } from './profession-detail.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ProfessionService } from 'app/profession.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
+import { Profession } from 'app/Profession';
+import { FormsModule } from '@angular/forms';
 
-describe("ProfessionDetailComponent", () => {
+describe('ProfessionDetailComponent', () => {
   let component: ProfessionDetailComponent;
   let fixture: ComponentFixture<ProfessionDetailComponent>;
+  let serviceSpy: jasmine.SpyObj<ProfessionService>;
+  let routerSpy: jasmine.SpyObj<Router>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ProfessionDetailComponent]
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule, FormsModule ],
+      declarations: [ ProfessionDetailComponent ],
+      providers: [
+        { provide: ProfessionService, useValue: jasmine.createSpyObj('ProfessionService', ['getProfession', 'updateProfession', 'deleteProfession']) },
+        { provide: Router, useValue: jasmine.createSpyObj('Router', ['navigate']) },
+        { provide: ActivatedRoute, useValue: { snapshot: { paramMap: { get: () => '1' } }} } 
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     }).compileComponents();
-  }));
-
-  beforeEach(() => {
+    
     fixture = TestBed.createComponent(ProfessionDetailComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    
+    serviceSpy = TestBed.inject(ProfessionService) as jasmine.SpyObj<ProfessionService>;
+    routerSpy = TestBed.inject(Router) as jasmine.SpyObj<Router>;
   });
 
-  it("should create", () => {
+  it('should create the Profession Detail page', () => {
+    serviceSpy.getProfession.and.returnValue(of({ observedOccupation: 'Clinic', serviceDiscipline: 'Doctor' }));
+    fixture.detectChanges(); //* Start up the component, running ngOnInit()
     expect(component).toBeTruthy();
+    expect(routerSpy).toBeTruthy();
+    expect(serviceSpy).toBeTruthy();
   });
+  it('should run router.navigate() after the back button is clicked', () => {
+    serviceSpy.getProfession.and.returnValue(of({ observedOccupation: 'Clinic', serviceDiscipline: 'Doctor' }));
+    fixture.detectChanges();
+    
+    const backButton = fixture.debugElement.query(By.css('.container > button'));
+    backButton.triggerEventHandler('click');
+    expect(routerSpy.navigate).toHaveBeenCalledOnceWith(['professions']);
+  })
+  it('should fetch the profession on init', fakeAsync(() => {
+    serviceSpy.getProfession.and.returnValue(of({ observedOccupation: 'Foobar', serviceDiscipline: 'Barfoo' }));
+    fixture.detectChanges();
+    tick();
+
+    const header = fixture.debugElement.query(By.css('div > h2')).nativeElement;
+    //? Angular's templates sometimes add whitespace to textContent (even if it doesn't render in prod that way) so trim() helps for comparisons
+    expect(header.textContent.trim()).toBe('Would you like to edit this Profession?: Foobar Barfoo');
+
+    const occupationInput: HTMLInputElement = fixture.debugElement.query(By.css('input[placeholder~="Occupation"]')).nativeElement;
+    const disciplineInput: HTMLInputElement = fixture.debugElement.query(By.css('input[placeholder~="Discipline"]')).nativeElement;
+    expect(occupationInput.value).toBe('Foobar');
+    expect(disciplineInput.value).toBe('Barfoo');
+  }))
+  it('should render an error message if no profession is fetched', () => {
+    const returnVal: any = undefined; //* Casting 'undefined' to 'any' or 'unknown' lets me to cast it to Profession below
+    serviceSpy.getProfession.and.returnValue(of(returnVal as Profession)); //* So I can imitate the professionService error handler returning undefined
+    fixture.detectChanges();
+
+    const errorHeader = fixture.debugElement.query(By.css('div > h3')).nativeElement;
+    expect(errorHeader.textContent).toBe('Unable to retrieve the proper profession');
+  })
+  it('should call updateProfession() and router.navigate() after pressing the update button', fakeAsync(() => {
+    serviceSpy.getProfession.and.returnValue(of({ observedOccupation: 'Clinic', serviceDiscipline: 'Doctor' }));
+    //* If updateProfession()'s returnVal of() stream is empty, the component's subscribe() would never run WHICH
+    serviceSpy.updateProfession.and.returnValue(of({ })); //* fails the test since router.navigate() inside wouldn't be run
+    fixture.detectChanges();
+    tick();
+
+    const occupationInput: HTMLInputElement = fixture.debugElement.query(By.css('input[placeholder~="Occupation"]')).nativeElement;
+    const disciplineInput: HTMLInputElement = fixture.debugElement.query(By.css('input[placeholder~="Discipline"]')).nativeElement;
+    occupationInput.value = 'Hospital';
+    disciplineInput.value = 'Nurse';
+    //* Since a '.value' update occurs from view/template side, need to dispatch an event so ngModel can update the component side
+    occupationInput.dispatchEvent(new Event('input')); //* Specifically an 'input' event!
+    disciplineInput.dispatchEvent(new Event('input'));
+
+    fixture.debugElement.query(By.css('span > button.btn.btn-warning')).triggerEventHandler('click');
+    expect(serviceSpy.updateProfession).toHaveBeenCalledOnceWith('1', { observedOccupation: 'Hospital', serviceDiscipline: 'Nurse' });
+    expect(routerSpy.navigate).toHaveBeenCalledOnceWith(['professions']);
+  }))
+  it('should call delete() and router.navigate() after pressing the delete button', fakeAsync(() => {
+    serviceSpy.getProfession.and.returnValue(of({ observedOccupation: 'Clinic', serviceDiscipline: 'Doctor' }));
+    serviceSpy.deleteProfession.and.returnValue(of({ observedOccupation: 'Clinic', serviceDiscipline: 'Doctor' }));
+    fixture.detectChanges();
+    tick();
+
+    fixture.debugElement.query(By.css('span > button.btn.btn-danger')).triggerEventHandler('click');
+    expect(serviceSpy.deleteProfession).toHaveBeenCalledOnceWith('1');
+    expect(routerSpy.navigate).toHaveBeenCalledOnceWith(['professions']);
+  }))
 });
--- src/app/profession-detail/profession-detail.component.ts
+++ src/app/profession-detail/profession-detail.component.ts
@@ -1,44 +1,43 @@
-import { Component, OnInit, Input, inject } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { Location } from "@angular/common";
-import { ProfessionService } from "../profession.service";
-import type { Profession } from "../Profession";
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProfessionService } from '../profession.service';
+import type { Profession } from '../Profession';
 
 @Component({
-  selector: "profession-detail",
-  templateUrl: "./profession-detail.component.html",
-  styleUrls: ["./profession-detail.component.scss"]
+  selector: 'profession-detail',
+  templateUrl: './profession-detail.component.html',
+  styleUrls: ['./profession-detail.component.scss']
 })
 export class ProfessionDetailComponent implements OnInit {
-  private route = inject(ActivatedRoute); //? Used to check the URL params i.e. "ID" in this component
+  private route = inject(ActivatedRoute); //? Used to check the URL params i.e. 'ID' in this component
+  private router = inject(Router);
   private professionService = inject(ProfessionService);
-  private location = inject(Location); //? Interacts with the browser -> Used to ensure back button works properly
 
-  @Input() profession: Profession = { observedOccupation: "Clinic", serviceDiscipline: "Doctor" };
+  profession: Profession = { observedOccupation: 'Clinic', serviceDiscipline: 'Doctor' };
 
-  ngOnInit(): void { //? Use this lifecycle method for loading data (let the constructor set simple local vars)
+  ngOnInit(): void { //? Use this lifecycle method for loading data. Let inject() set class properties above
     this.getProfession();
   } //? If data directives involved, then use ngOnChanges (which is called before ngOnInit)
 
   getProfession(): void {
-    const id = this.route.snapshot.paramMap.get("id") ?? "";
+    const id = this.route.snapshot.paramMap.get('id') ?? '';
     //? W/out subscribe, service will not make a request. W/out its callback, nothing will happen with the returned data
     this.professionService.getProfession(id).subscribe(profession => {
       this.profession = profession;
     });
   }
   update(): void {
-    const id = this.route.snapshot.paramMap.get("id") ?? "";
+    const id = this.route.snapshot.paramMap.get('id') ?? '';
     this.professionService
       .updateProfession(id, this.profession)
-      .subscribe(() => this.goBack());
+      .subscribe(() => this.navToProfessionsList());
   }
   delete(): void {
-    const id = this.profession._id ?? "";
-    this.professionService.deleteProfession(id).subscribe(() => this.goBack());
+    const id = this.route.snapshot.paramMap.get('id') ?? '';
+    this.professionService.deleteProfession(id).subscribe(() => this.navToProfessionsList());
   }
 
-  goBack(): void {
-    this.location.back();
+  navToProfessionsList(): void {
+    this.router.navigate(['professions']);
   }
 }
