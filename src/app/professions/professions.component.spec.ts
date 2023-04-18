281210c3c3df80a4c441c18a03d0aab505bad71a
NLCaceres <NLCaceres21@Gmail.com>
2023-04-18 12:51:54 -0700

    ProfessionList component better styled + linted & tested

--- src/app/professions/professions.component.spec.ts
+++ src/app/professions/professions.component.spec.ts
@@ -1,24 +1,71 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProfessionsComponent } from './professions.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ProfessionService } from 'app/profession.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Subscription, of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 describe('ProfessionsComponent', () => {
   let component: ProfessionsComponent;
   let fixture: ComponentFixture<ProfessionsComponent>;
+  let serviceSpy: jasmine.SpyObj<ProfessionService>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ProfessionsComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      declarations: [ ProfessionsComponent ],
+      providers: [ { provide: ProfessionService, useValue: jasmine.createSpyObj('ProfessionService', ['getAllProfessions']) } ],
+      schemas: [ NO_ERRORS_SCHEMA ]
+    }).compileComponents();
+    
     fixture = TestBed.createComponent(ProfessionsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    serviceSpy = TestBed.inject(ProfessionService) as jasmine.SpyObj<ProfessionService>;
   });
+  
+  it('should create a Profession List Page', () => {
+    serviceSpy.getAllProfessions.and.returnValue(of([{ observedOccupation: 'Foobar', serviceDiscipline: 'Barfoo' }]));
+    fixture.detectChanges();
+
+    expect(component).toBeTruthy();
+    expect(serviceSpy).toBeTruthy();
+  });
+  it('should fetch professions and render a Titlecased list item for each on init', () => {
+    serviceSpy.getAllProfessions.and.returnValue(of([
+      { observedOccupation: 'foobar', serviceDiscipline: 'barfoo' }, { observedOccupation: 'fizz', serviceDiscipline: 'buzz' }
+    ]));
+    fixture.detectChanges();
+
+    const professionListItems = fixture.debugElement.queryAll(By.css('li'));
+    expect(professionListItems.length).toBe(2);
+
+    //* Must burrow down from <li> level -> <a> -> <span> {{ text }} </span>
+    const firstProfession: HTMLElement = professionListItems[0].children[0].children[0].nativeElement;
+    expect(firstProfession.textContent?.trim()).toBe('Foobar Barfoo');
+    const secondProfession: HTMLElement = professionListItems[1].children[0].children[0].nativeElement;
+    expect(secondProfession.textContent?.trim()).toBe('Fizz Buzz');
+  })
+  it('should calculate the viewWidth on init so bigger viewports can get a sidebar', () => {
+    serviceSpy.getAllProfessions.and.returnValue(of([{ observedOccupation: 'Foobar', serviceDiscipline: 'Barfoo' }]));
+    fixture.detectChanges();
+
+    //? ViewWidth = 1200 based on default size of the Chrome test result browser that appears while running tests MEANWHILE component default = 1024
+    component.viewWidth = 576; //* As a precaution, set the viewWidth anyway
+    fixture.detectChanges(); //* And ensure the changes take place
+    expect(fixture.debugElement.query(By.css('sidebar'))).toBeDefined();
+
+    component.viewWidth = 575;
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('sidebar'))).toBeNull();
+  })
+  it('should unsubscribe from viewWidth changes on destroy', () => {
+    serviceSpy.getAllProfessions.and.returnValue(of([{ observedOccupation: 'Foobar', serviceDiscipline: 'Barfoo' }]));
+    fixture.detectChanges(); //? Important for spyOn to come after detectChanges so the subscription has been init in ngOnInit
+    const unsubscribeSpy = spyOn(component.resizeSubscription$, 'unsubscribe').and.callThrough();
+
+    component.ngOnDestroy(); //* Now unsubscribe to prevent any memory leaks
+    expect(unsubscribeSpy).toHaveBeenCalledTimes(1);
+  })
 });
--- src/app/professions/professions.component.ts
+++ src/app/professions/professions.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit, OnDestroy, inject } from "@angular/core";
-import { Profession } from "../Profession";
-import { ProfessionService } from "../profession.service";
-import { debounceTime, fromEvent, Subscription } from "rxjs";
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
+import { Profession } from '../Profession';
+import { ProfessionService } from '../profession.service';
+import { debounceTime, fromEvent, Subscription } from 'rxjs';
 
 @Component({
-  selector: "profession-list",
-  templateUrl: "./professions.component.html",
-  styleUrls: ["./professions.component.scss"]
+  selector: 'profession-list',
+  templateUrl: './professions.component.html',
+  styleUrls: ['./professions.component.scss']
 })
 export class ProfessionsComponent implements OnInit, OnDestroy { //? Just like Java or Swift
-  viewWidth = 1024;
   professionService = inject(ProfessionService); //? Easy, super readable alternative to constructor injection
   professions: Profession[] = []; //? Visible to the HTML template
-  resizeSubscription$?: Subscription
+  resizeSubscription$!: Subscription; //? Using '!' and not '?' guarantees this optional property will be set before usage
+  viewWidth = 1024;
 
   //! Lifecycle methods
   ngOnInit() {
@@ -20,13 +20,13 @@ export class ProfessionsComponent implements OnInit, OnDestroy { //? Just like J
     this.resizeSubscription$ = fromEvent(window, 'resize').pipe(debounceTime(300)).subscribe(event => {
       this.viewWidth = (event.target as Window).innerWidth;
     })
-    this.getProfession();
+    this.getProfessions();
   }
   ngOnDestroy() {
-    this.resizeSubscription$?.unsubscribe()
+    this.resizeSubscription$.unsubscribe();
   }
 
-  getProfession(): void { //? Since I'm dealing with an observable, must subscribe and handle the awaited result
+  private getProfessions(): void { //? Since I'm dealing with an observable, must subscribe and handle the awaited result
     this.professionService.getAllProfessions().subscribe(professions => {
       this.professions = professions;
     });
