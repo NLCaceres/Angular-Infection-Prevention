58eabcdbbb588a971cac78ee34e49130bfa51e76
NLCaceres <NLCaceres21@Gmail.com>
2023-04-21 13:29:57 -0700

    Clean template layout + component & template props

--- src/app/sidebar/sidebar.component.spec.ts
+++ src/app/sidebar/sidebar.component.spec.ts
@@ -1,24 +1,59 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, flush, tick } from '@angular/core/testing';
 import { SidebarComponent } from './sidebar.component';
+import { ProfessionService } from 'app/profession.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
+  let serviceSpy: jasmine.SpyObj<ProfessionService>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SidebarComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule, RouterTestingModule, //? Enables routerLink props w/out requiring any injections
+        FormsModule, NgbTypeaheadModule ], //? NgbTypeahead declares relevant components for testing  
+      declarations: [ SidebarComponent ],
+      providers: [ { provide: ProfessionService, useValue: jasmine.createSpyObj('ProfessionService', ['searchProfessions']) } ],
+      
+    }).compileComponents();
+    
     fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
+
+    serviceSpy = TestBed.inject(ProfessionService) as jasmine.SpyObj<ProfessionService>;
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create a sidebar component', () => {
+    expect(component).toBeTruthy(); //* Tough to say if toBeDefined() or toBeTruthy() is better
+    expect(serviceSpy).toBeDefined(); //* Truthy would probably only ever fail if the value had to be 0 or false
   });
+  it('should format the profession occupation and discipline into a readable string', () => {
+    const professionName = component.formatter({ observedOccupation: 'Foobar', serviceDiscipline: 'Barfoo' });
+    expect(professionName).toBe('Foobar Barfoo');
+  })
+  it('should use any search terms input to make filtered requests from the server', fakeAsync(() => {
+    serviceSpy.searchProfessions.and.returnValues(
+      of([{ observedOccupation: 'Foobar', serviceDiscipline: 'Barfoo'}]), 
+      of([{ observedOccupation: 'Fizz', serviceDiscipline: 'Buzz'}, { observedOccupation: 'Faz', serviceDiscipline: 'Baz' }]),
+    );
+    component.search(of('foobar')).subscribe(list => {
+      expect(list).toEqual([{ observedOccupation: 'Foobar', serviceDiscipline: 'Barfoo' }]);
+    });
+
+    const inputElem: HTMLInputElement = fixture.debugElement.query(By.css('input')).nativeElement;
+    inputElem.value = 'barfoo';
+    inputElem.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    tick(350); //* Must elapse the debounce period to ensure ngb-highlight list appears under searchbar
+
+    expect(fixture.debugElement.queryAll(By.css('ngb-highlight'))).toHaveSize(2);
+    flush(); //* Must clear async tasks to pass test
+  }))
 });
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,26 +1,24 @@
-import { Component, inject } from "@angular/core";
-import { Observable } from "rxjs";
-import { debounceTime, distinctUntilChanged, switchMap } from "rxjs/operators";
-import { Profession } from "../Profession";
-import { ProfessionService } from "../profession.service";
+import { Component, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { Profession } from '../Profession';
+import { ProfessionService } from '../profession.service';
 
 @Component({
-  selector: "sidebar",
-  templateUrl: "./sidebar.component.html",
-  styleUrls: ["./sidebar.component.scss"]
+  selector: 'sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
   private professionService = inject(ProfessionService);
-  
-  public professions: Profession[] = [];
 
-  search = (term$: Observable<string>) => //? Why the "$" suffix? Convention denotes an observable
+  search = (term$: Observable<string>) => //? The '$' suffix is an Angular convention denoting observables
     term$.pipe(
-      debounceTime(300), //? Wait 300 ms after each keystroke before considering a search term
+      debounceTime(300), //? MUST observe a 300 ms pause before considering a search term
       distinctUntilChanged(), //? Ignore new term if same as previous search term
-      switchMap((term: string) => //? switchMap takes in each new observable sent by the service
-        this.professionService.searchProfessions(term) //? Preserving order of HTTP Request results emitted by those observables
-      ) //? BUT it will not cancel http requests, it simply discards the results
+      switchMap((term: string) => { //? switchMap takes in each new observable sent by the service
+        return this.professionService.searchProfessions(term) //? Preserving order of HTTP Request results emitted by those observables
+      }) //? BUT it will not cancel http requests, it simply discards previous results in favor of the most recent
     );
   formatter = (profession: Profession) => {
     return `${profession.observedOccupation} ${profession.serviceDiscipline}`;
