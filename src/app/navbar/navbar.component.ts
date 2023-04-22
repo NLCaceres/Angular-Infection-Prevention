81ecebe5b295cf6c0778276b634d2c63f0057cd9
NLCaceres <NLCaceres21@Gmail.com>
2023-04-22 12:20:12 -0700

    Lint and test navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,15 @@
-import { Component } from "@angular/core";
-import { NavItem } from "../NavItem";
+import { Component } from '@angular/core';
+import { NavItem } from '../NavItem';
 
 @Component({
-  selector: "navbar",
-  templateUrl: "./navbar.component.html",
-  styleUrls: ["./navbar.component.scss"]
+  selector: 'navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
   isCollapsed = true;
   navBrand: NavItem = {
-    title: "Infection Protection"
+    title: 'Infection Protection'
   };
 
   toggleMenu(collapse?: boolean) {
