9c91674892b6586dfff6804b79571c538cf967ee
NLCaceres <NLCaceres21@Gmail.com>
2024-06-27 23:49:19 -0700

    Start converting interfaces to TS types + move to `models` directory - Converting to TS types to match ESLint config   - TS Types have many of the same features and performance of TS interfaces   while also preventing unexpected declaration merging

--- src/app/models/Profession.ts
+++ src/app/models/Profession.ts
@@ -1,5 +1,5 @@
-export interface Profession {
+export type Profession = {
   _id?: string; // Not technically optional BUT it won't have an ID until the server defines it
   observedOccupation: string;
   serviceDiscipline: string;
-}
+};
