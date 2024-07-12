4b4fbbaa43a7fa2fa2f5b405243845794fd1feb9
NLCaceres <NLCaceres21@Gmail.com>
2024-07-12 14:46:40 -0700

    Move `MessageService` into new `Services` folder

--- src/app/services/message.service.spec.ts
+++ src/app/services/message.service.spec.ts
@@ -4,7 +4,7 @@ import { MessageService } from "./message.service";
 describe("MessageService", () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it("should be created", () => { //* Easy simple sanity check for TestBed
+  it("should be created", () => { // - Easy simple sanity check for TestBed
     const service: MessageService = TestBed.inject(MessageService);
     expect(service).toBeTruthy();
   });
--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
 
 @Injectable({
-  providedIn: "root" //? No need to put this messageService in provider array
+  providedIn: "root" // ?: No need to put this messageService in provider array
 })
 export class MessageService {
   message$ = new Subject<string>();
